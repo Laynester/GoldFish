af095ef6ad7bd3ea9072a86c258fba0662179f3c
ObjectRetros <dennisobject@gmail.com>
2021-08-11 23:15:26 +0200

    Correct url link, to be more explaining

--- resources/views/nitro.blade.php
+++ resources/views/nitro.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Goldfish - Nitro</title>
+    <title>{{ ENV('APP_NAME') }} - Nitro</title>
 </head>
 <body>
-    <iframe src="{{ ENV('APP_URL') }}/client/html5/index.html?sso={{$sso}}" frameborder="0" style="overflow:hidden;height:100%;width:100%;margin:0;padding:0;position:absolute;top:0;left:0;"></iframe>
+    <iframe src="{{ ENV('APP_URL') }}/URL-TO-NITRO-INDEX-FILE?sso={{$sso}}" frameborder="0" style="overflow:hidden;height:100%;width:100%;margin:0;padding:0;position:absolute;top:0;left:0;"></iframe>
 <div id=app></div>
 </body>
 </html>
