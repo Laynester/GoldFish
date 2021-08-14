90d6e7d7f0493f36ee5a32987afca3e651ce950b
ObjectRetros <dennisobject@gmail.com>
2021-08-14 02:01:07 +0200

    updated legacy footer

--- resources/views/Legacy/components/footer.blade.php
+++ resources/views/Legacy/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
   <p></p>
-  <p>{{CMSHelper::settings('hotelname')}} is Powered by <a href="https://discord.gg/eVAYDUp">GoldFish</a> <b>v{{config('app.version_number')}}</b> Made with <i class="icon ion-heart"></i> © 2019 <a href="http://layne.cf">Laynester</a></p>
+  <p>{{CMSHelper::settings('hotelname')}} is Powered by <a href="https://discord.gg/eVAYDUp">GoldFish</a> <b>v{{config('app.version_number')}}</b>, current Laravel version v{{ Illuminate\Foundation\Application::VERSION }} - Made with <i class="icon ion-heart"></i> by <a href="https://devbest.com/members/laynester.83341/">Laynester</a> Further updates by <a href="https://devbest.com/members/object.78351/">Object</a><br></p>
   <p>HABBO is a registered trademark of Sulake Corporation. All rights reserved to their respective owner(s).</p>
 </footer>
