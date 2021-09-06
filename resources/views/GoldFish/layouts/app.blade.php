1c062cdf6ffc7b6f9ff5bee0535220520dba72f5
ObjectRetros <dennisobject@gmail.com>
2021-09-06 22:13:39 +0200

    Remove wrong bootstrap version

--- resources/views/GoldFish/layouts/app.blade.php
+++ resources/views/GoldFish/layouts/app.blade.php
@@ -17,8 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
