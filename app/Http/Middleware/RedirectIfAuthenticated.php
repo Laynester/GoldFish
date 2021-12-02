a4256acbc8818e40b3aab3a3b3ce279dbfa66793
ObjectRetros <dennisobject@gmail.com>
2021-12-02 22:09:41 +0100

    Fix auth redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/me');
+            return redirect()->route('me.index');
         }
         return $next($request);
     }
