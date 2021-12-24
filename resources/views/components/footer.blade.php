bf709c42a91f0e4d6027514450db48f64e5f5589
ObjectRetros <87041394+ObjectRetros@users.noreply.github.com>
2021-12-24 03:27:26 +0100

    Update footer (#37)

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,15 @@
 <div class="container">
     <footer class="home">
-        {{ __('Powered by Goldfish CMS') }} <strong>v{{config('app.version_number')}}</strong>
-        <a href="https://discord.gg/eVAYDUp">{{ __('Goldfish Discord') }}</a>
-        - {{ __('Current Laravel version') }} {{ Illuminate\Foundation\Application::VERSION }}.
+        {{ __('Goldfish CMS') }} <strong>v{{ config('app.version_number') }}</strong> -
         {{ __('Made with') }} <i class="icon ion-heart"></i> {{ __('by') }}
         <a href="https://devbest.com/members/laynester.83341/">Laynester</a>
-        {{ __('Further updates by') }} <a href="https://devbest.com/members/object.78351/">Object</a><br>
+        {{ __('and maintained by') }} <a href="https://devbest.com/members/object.78351/">Object</a>
+        <br>
         &copy {{ date('Y') }} {{ __('HABBO is a registered trademark of Sulake Corporation. All rights reserved to their respective owner(s).') }}
+        <br>
+        <span style="font-size: 12px;">
+            {{ __('Current Laravel version') }} v{{ Illuminate\Foundation\Application::VERSION }} - {{ __('Current PHP version') }} v{{ PHP_VERSION }}
+        </span>
+
     </footer>
 </div>
