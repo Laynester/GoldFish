cb261eac994ca6bcfb2b71f170fca1f48f7d650e
ObjectRetro <dennisobject@gmail.com>
2022-05-21 03:38:06 +0200

    Correct error in ban middleware if not logged in

--- app/Http/Middleware/Banned.php
+++ app/Http/Middleware/Banned.php
@@ -12,6 +12,10 @@ class Banned
 {
     public function handle($request, Closure $next)
     {
+        if (!Auth::check()) {
+            return $next($request);
+        }
+
         $isBanned = Bans::query()
             ->where('user_id', '=', auth()->id())
             ->orWhere('ip', '=', auth()->user()->ip_current)
