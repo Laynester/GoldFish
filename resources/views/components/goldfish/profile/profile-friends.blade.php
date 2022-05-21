a6f2a4e2d221c034a563cd340ca94e74b61dd182
ObjectRetro <dennisobject@gmail.com>
2022-05-21 03:40:31 +0200

    Fix profile page on GF theme

--- resources/views/components/goldfish/profile/profile-friends.blade.php
+++ resources/views/components/goldfish/profile/profile-friends.blade.php
@@ -3,9 +3,9 @@
 <div class="box blue profile friends">
     <div class="heading">{{ __(':username friends', ['username' => $user->username]) }}</div>
         @forelse($friends as $friend)
-            <a href="{{ route('profile.show', $friend->habbo->username) }}">
-                <figure class="friend" style="background-image:url({{CMSHelper::settings('habbo_imager')}}{{ $friend->habbo->look }}&direction=4&headonly=1);">
-                    {{$friend->habbo->username}}
+            <a href="{{ route('profile.show', $friend->friend->username) }}">
+                <figure class="friend" style="background-image:url({{CMSHelper::settings('habbo_imager')}}{{ $friend->friend->look }}&direction=4&headonly=1);">
+                    {{$friend->friend->username}}
                 </figure>
             </a>
         @empty
