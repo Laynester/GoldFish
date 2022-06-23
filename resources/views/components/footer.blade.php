713e9fb05a0156cfac79f16aee8dccab6f9f5f75
ObjectRetro <dennisobject@gmail.com>
2022-06-23 22:31:18 +0200

    Update footer content

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,9 @@
 <div class="container">
     <footer class="home">
-        {{ __('Goldfish CMS') }} <strong>v{{ config('app.version_number') }}</strong> -
-        {{ __('Made with') }} <i class="icon ion-heart"></i> {{ __('by') }}
-        <a href="https://devbest.com/members/laynester.83341/">Laynester</a>
-        {{ __('and maintained by') }} <a href="https://devbest.com/members/object.78351/">Object</a>
-        <br>
-        &copy {{ date('Y') }} {{ __('HABBO is a registered trademark of Sulake Corporation. All rights reserved to their respective owner(s).') }}
-        <br>
+        {{ __('Habbo is a registered trademark of Sulake Corporation. All rights reserved to their respective owner(s)') }}<br>
+        &copy {{ date('Y') }} {{ __(':hotel is a not for profit educational project and is in no way affiliated with Habbo or Sulake Corporation', ['hotel' => ENV('APP_NAME')]) }}<br>
         <span style="font-size: 12px;">
+            {{ __('Goldfish CMS') }} <strong>v{{ config('app.version_number') }}</strong>.
             {{ __('Current Laravel version') }} v{{ Illuminate\Foundation\Application::VERSION }} - {{ __('Current PHP version') }} v{{ PHP_VERSION }}
         </span>
 
